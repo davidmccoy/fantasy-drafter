85e50ae980f7310325f35bdc3d9410d2e0c974ed
David McCoy <david.mccoy@gmail.com>
2017-03-15 19:11:11 -0500

    renamed game type to game category. apparently type is a reserved word :rage:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315235458) do
+ActiveRecord::Schema.define(version: 20170315235853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170315235458) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
-    t.integer  "type",       default: 0, null: false
+    t.integer  "category",   default: 0, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
