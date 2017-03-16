8e7fa10aa061d8127e1a18b78b00d939c6dbea34
David McCoy <david.mccoy@gmail.com>
2017-03-15 21:12:44 -0500

    updated relationship between users, tournaments, and tournament users

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,7 +1,8 @@
 class Tournament < ApplicationRecord
 
   belongs_to :competition
-  belongs_to :admin, class_name: "User", foreign_key: "user_id"
+  has_many :tournament_users
   has_many :users, through: :tournament_users
+  belongs_to :admin, class_name: "User", foreign_key: "user_id"
 
 end
