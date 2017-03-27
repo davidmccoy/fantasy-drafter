7a4db81c90221895bf15192016df4300cff789d9
David McCoy <david.mccoy@gmail.com>
2017-03-27 18:22:54 -0500

    added players to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327220704) do
+ActiveRecord::Schema.define(version: 20170327232008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 20170327220704) do
     t.integer  "user_id"
     t.integer  "player_id"
     t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name"
+    t.string   "team"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
