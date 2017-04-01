a9557fd762834890a437cc4ff89ee4c493b28cf0
David McCoy <david.mccoy@gmail.com>
2017-03-31 21:19:46 -0500

    added a bit of security to making a pick

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -3,10 +3,14 @@ class PicksController < ApplicationController
   load_and_authorize_resource
 
   def update
-    if @pick.update(pick_params)
-      flash[:notice] = "Successfully drafted #{@pick.player.name}."
+    if @pick.user == current_user || current_user = @pick.draft.tournament.admin
+      if @pick.update(pick_params)
+        flash[:notice] = "Successfully drafted #{@pick.player.name}."
+      else
+        flash[:alert] = "Pick failed."
+      end
     else
-      flash[:alert] = "Pick failed."
+      flash[:alert] = "It's not your pick."
     end
     redirect_to game_competition_tournament_draft_path(@pick.draft.tournament.competition.game, @pick.draft.tournament.competition, @pick.draft.tournament, @pick.draft)
   end
