5eb52b3d8d301ef317884e7b74d4f3078e894f50
David McCoy <david.mccoy@gmail.com>
2017-03-31 19:08:26 -0500

    added draft show

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -2,7 +2,22 @@ class DraftsController < ApplicationController
 
   load_and_authorize_resource
 
-  def index
+  def show
+  end
+
+  def update
+    if @draft.update(draft_params)
+      flash[:notice] = "Successfully updated the draft start time."
+    else
+      flash[:alert] = "Update failed."
+    end
+    redirect_to game_competition_tournament_draft_path(@draft.tournament.competition.game, @draft.tournament.competition, @draft.tournament, @draft)
+  end
+
+  private
+
+  def draft_params
+    params.require(:draft).permit(:start_time)
   end
 
 end
