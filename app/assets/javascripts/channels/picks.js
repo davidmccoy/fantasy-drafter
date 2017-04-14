569d81288068aa42314cf4ec005f4a2ccfe07f61
David McCoy <david.mccoy@gmail.com>
2017-04-14 16:27:42 -0500

    implemented live drafting updates on show, js, and in controller

--- app/assets/javascripts/channels/picks.js
+++ app/assets/javascripts/channels/picks.js
@@ -1,7 +1,27 @@
 App.messages = App.cable.subscriptions.create('PicksChannel', {
   received: function(data) {
-    $("#picks").removeClass('hidden')
-    return $('#picks').append(this.renderPick(data));
+    $("#picks").removeClass('hidden');
+    $pick = $('p#pick-' + data.number);
+    $player = $('p#' + data.player_id);
+    $currentPick = $('#current-pick');
+    $pickLinks = $('.pick-link');
+
+
+    $pick.text($pick.text() + ' || ' + data.player_name).attr('style', 'color: gray');
+    $player.remove();
+    $currentPick.text('Current Pick: ' + data.next_pick_user_name + ' (Pick #' + data.next_pick_number + ')');
+
+    $pickLinks.each(function(index) {
+      playerId = $(this).attr('href').split('?player_id=')[1];
+      $(this).attr('href', data.next_pick_url + '?player_id=' + playerId);
+
+      if (data.your_pick === false) {
+        $(this).hide();
+      }
+    })
+
+    console.log(data.your_pick);
+    // return $('#picks').append(this.renderPick(data));
   },
 
   renderPick: function(data) {
