fade4057356ae66c3c8a5024a56b0dd2d3c76273
David McCoy <david.mccoy@gmail.com>
2017-04-14 17:07:27 -0500

    added production config info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,10 @@ Rails.application.configure do
 
   # Configure mailer for production
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Set Action Cable url
+  config.web_socket_server_url = "wss:/fantasy-drafter.herokuapp.com/cable"
+
+  # Allow origin requests
+  config.action_cable.allowed_request_origins = ['https://fantasy-drafter.herokuapp.com', 'http://fantasy-drafter.herokuapp.com']
 end
