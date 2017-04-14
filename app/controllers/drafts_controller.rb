d1327b91f7226bfbace99c2f301cd66ace96557e
David McCoy <david.mccoy@gmail.com>
2017-04-14 17:40:11 -0500

    draft start time fix

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -3,7 +3,7 @@ class DraftsController < ApplicationController
   load_and_authorize_resource
 
   def show
-    if @draft.active || Time.now > @draft.start_time
+    if @draft.start_time && (@draft.active || Time.now > @draft.start_time)
       @available_players = @draft.tournament.competition.players.where.not(id: @draft.picks.pluck(:player_id) )
 
       @current_pick = @draft.picks.where(player_id: nil).sort_by{ |pick| pick.number }.first
