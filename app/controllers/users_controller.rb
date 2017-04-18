78bc266ddf2f9f7c15cd9ec7bdf66ae7c7aa76ae
David McCoy <david.mccoy@gmail.com>
2017-04-18 16:59:20 -0500

    added user profile and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  load_and_authorize_resource
+
+  def show
+  end
+
   def leagues
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :users
+
   devise_scope :user do
     match '/user/leagues', to: 'users#leagues', via: [:get]
   end
