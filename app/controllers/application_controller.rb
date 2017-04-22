3cf3f0416b99b831dee8f4c0bb5266ab3c7ac154
David McCoy <david.mccoy@gmail.com>
2017-04-21 20:51:35 -0500

    added html safe flash messages to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  before_filter -> { flash.now[:notice] = flash[:notice].html_safe if flash[:html_safe] && flash[:notice] }
+
+  before_filter -> { flash.now[:alert] = flash[:alert].html_safe if flash[:html_safe] && flash[:alert] }
+  
 end
