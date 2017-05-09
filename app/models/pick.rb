5c29a0d3e310fdc70995e160e7ce8b2b6179e659
David McCoy <david.mccoy@gmail.com>
2017-05-09 17:39:10 -0500

    added table to draft view

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -5,4 +5,16 @@ class Pick < ApplicationRecord
   belongs_to :player
   has_one :user, through: :team
 
+  # To allow users to delete their account while we maintain their stats
+  def user
+    puts self.id
+    user = User.find_by_id(self.team.user)
+
+    if user
+      return user
+    else
+      return User.new(name: "Deleted User")
+    end
+  end
+
 end
