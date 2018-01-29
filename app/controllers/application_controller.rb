e7da12d4d29ff8db40c213333577bbfc52d70026
David McCoy <david.mccoy@gmail.com>
2018-01-29 13:04:13 -0500

    changed before_filter to before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  before_filter -> { flash.now[:notice] = flash[:notice].html_safe if flash[:html_safe] && flash[:notice] }
+  before_action -> { flash.now[:notice] = flash[:notice].html_safe if flash[:html_safe] && flash[:notice] }
 
-  before_filter -> { flash.now[:alert] = flash[:alert].html_safe if flash[:html_safe] && flash[:alert] }
-  
+  before_action -> { flash.now[:alert] = flash[:alert].html_safe if flash[:html_safe] && flash[:alert] }
+
 end
