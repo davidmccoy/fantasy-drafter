edccbca134e3bc208ad236d975b27edd7b1f1aec
David McCoy <david.mccoy@gmail.com>
2018-05-29 11:44:37 -0500

    league association with invite

--- app/models/league.rb
+++ app/models/league.rb
@@ -6,6 +6,7 @@ class League < ApplicationRecord
   belongs_to :admin, class_name: "User", foreign_key: "user_id"
   has_one :draft, dependent: :destroy
   has_many :teams, through: :league_users
+  has_many :invites
 
   accepts_nested_attributes_for :draft
 
