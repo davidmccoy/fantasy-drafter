dd7181e42d8bc18a12c91bb909760d846b954598
David McCoy <david.mccoy@gmail.com>
2018-09-17 09:46:26 -0500

    added contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :players, only: [:new, :import]
   post 'players/import', to: 'players#import'
   resource :faq
+  resource :contact_us, controller: 'contact_us'
 
   resources :games, param: :slug, only: [:index, :new] do
     resources :competitions, param: :slug, only: [:index, :show] do
