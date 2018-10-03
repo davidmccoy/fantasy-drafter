58530c77a2a2739cd3fbf7210be4f2e6161d3745
David McCoy <david.mccoy@gmail.com>
2018-10-03 16:38:20 -0500

    updates

--- app/mailers/league_mailer.rb
+++ app/mailers/league_mailer.rb
@@ -5,11 +5,21 @@ class LeagueMailer < ApplicationMailer
     @league = league
     @league_admin = league.admin
     @user = user
-    @url = Rails.application.routes.url_helpers.game_competition_league_url(
+    leagueable_class = @leagueable.class
+
+    if leagueable_class == Season
+      @url = Rails.application.routes.url_helpers.season_league_url(
+        game_slug: @league.leagueable.game.slug,
+        season_slug: @league.leagueable.slug,
+        id: @league.id
+      )
+    elsif if leagueable_class == Competition
+      @url = Rails.application.routes.url_helpers.game_competition_league_url(
         game_slug: @league.leagueable.game.slug,
         competition_slug: @league.leagueable.slug,
         id: @league.id
       )
+    end
 
     mail(to: @league_admin.email, subject: "#{@user.name} has accepted their invite to your #{@league.leagueable.name} Fantasy Draft!")
   end
@@ -18,11 +28,21 @@ class LeagueMailer < ApplicationMailer
     @league = league
     @league_admin = league.admin
     @user = user
-    @url = Rails.application.routes.url_helpers.game_competition_league_url(
+    leagueable_class = @leagueable.class
+
+    if leagueable_class == Season
+      @url = Rails.application.routes.url_helpers.game_season_league_url(
+        game_slug: @league.leagueable.game.slug,
+        season_slug: @league.leagueable.slug,
+        id: @league.id
+      )
+    elsif if leagueable_class == Competition
+      @url = Rails.application.routes.url_helpers.game_competition_league_url(
         game_slug: @league.leagueable.game.slug,
         competition_slug: @league.leagueable.slug,
         id: @league.id
       )
+    end
 
     mail(to: @league_admin.email, subject: "#{@user.name} has declined their invite to your #{@league.leagueable.name} Fantasy Draft.")
   end
