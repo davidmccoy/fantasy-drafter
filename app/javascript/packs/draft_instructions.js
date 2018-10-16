7add243276f30b72a647643f64808c0c3c360cc8
David McCoy <david.mccoy@gmail.com>
2018-10-16 18:12:01 -0500

    add clicking for more info to instructions

--- app/javascript/packs/draft_instructions.js
+++ app/javascript/packs/draft_instructions.js
@@ -78,10 +78,10 @@ class DraftInstructions extends React.Component {
                 The "ELO" column lists the player's ELO rating, which is a scoring system where every player starts with a rating of 1500. After each match, the winning player takes points from the losing player. The amount of points at stake is determined by the difference in the players' ratings.
               </p>
               <p>
-                Click on a player's name to view more information about them.
+                You can sort each column by clicking on it. You can also add secondary  sorting by choosing the main column you'd like sort by, then holding the SHIFT key and selecting the column you'd like to use as a secondary sort.
               </p>
               <p>
-                You can sort each column by clicking on it. You can also add secondary  sorting by choosing the main column you'd like sort by, then holding the SHIFT key and selecting the column you'd like to use as a secondary sort.
+                Click on a player's name to view more information about them.
               </p>
               <p>
                 Click the <button className="btn btn-sm star-link"><img src={starGold} className="star-gold-image"/><img src={star} className="star-image" /></button> to save a list of your favorite players to pick when it's your turn. Click the <button className="btn btn-sm btn-success pick-link"><img src={plus} /></button> to draft a player and add them to your team.
